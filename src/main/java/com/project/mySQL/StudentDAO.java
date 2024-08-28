9599499cd9ed8c9cb3a2634d009739de5a5285f9
alexKamo <sashakamor@gmail.com>
2024-08-27 20:32:07 -0500

    mySQL

--- src/main/java/com/project/mySQL/StudentDAO.java
+++ src/main/java/com/project/mySQL/StudentDAO.java
@@ -4,4 +4,5 @@ public interface StudentDAO {
 
     void save (Student student);
 
+    Student findById(Integer id);
 }
--- src/main/java/com/project/mySQL/StudentDAOImpl.java
+++ src/main/java/com/project/mySQL/StudentDAOImpl.java
@@ -20,4 +20,9 @@ public class StudentDAOImpl implements StudentDAO{
     public void save(Student student) {
         entityManager.persist(student);
     }
+
+    @Override
+    public Student findById(Integer id) {
+       return entityManager.find(Student.class,id);
+    }
 }
